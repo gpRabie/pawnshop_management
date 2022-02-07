1751490402c1b82ce79886ff230ed521843bb1c5
Rabie <gprabiemosessantillan@gmail.com>
2022-02-07 11:37:59 +0800

    Role change v.4

--- pawnshop_management/pawnshop_management/doctype/role_assignment/role_assignment.py
+++ pawnshop_management/pawnshop_management/doctype/role_assignment/role_assignment.py
@@ -6,7 +6,19 @@ from frappe.model.document import Document
 
 class RoleAssignment(Document):
 	def before_save(self):
-		reset_roles()
+		# reset_roles()
+		users = frappe.db.get_list('User', pluck='name')
+		for i in range(len(users)):
+			if users[i] != "Administrator":
+				user_role_change = frappe.get_doc('User', users[i])
+				if user_role_change.role_profile_name == 'Supervisor/Cashier':
+					user_role_change.role_profile_name = 'Supervisor'
+				elif user_role_change.role_profile_name == 'Appraiser/Cashier':
+					user_role_change.role_profile_name = 'Appraiser'
+				elif user_role_change.role_profile_name == 'Cashier':
+					user_role_change.role_profile_name = 'Guest'
+				user_role_change.save(ignore_permissions=True)
+
 		user = frappe.get_doc('User', self.employee)
 		if user.role_profile_name == 'Appraiser':
 			user.role_profile_name = 'Appraiser/Cashier'
