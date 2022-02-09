4170d2f9c2c6686054f475246316c1c47786e7a4
Rabie <gprabiemosessantillan@gmail.com>
2022-02-09 11:26:37 +0800

    Auto change pawn Ticket Status

--- pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.py
+++ pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2022, Rabie Moses Santillan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ProvisionalReceipt(Document):
-	pass
+	def before_submit(self):
+		if self.transaction_type == "Redemption":
+			doc = frappe.get_doc('Pawn Ticket Non Jewelry', self.pawn_ticket_no)
+			doc.workflow_state = "Redeemed"
+			doc.save()
+			frappe.db.commit()
