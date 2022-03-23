08b08ae28cd4675aa97def4735f79d53ba6df55f
Rabie <gprabiemosessantillan@gmail.com>
2022-03-23 13:37:59 +0800

    Remove mandatory property for branch in NJ Pawn Ticket

--- pawnshop_management/pawnshop_management/custom_codes/security.py
+++ pawnshop_management/pawnshop_management/custom_codes/security.py
@@ -3,5 +3,7 @@ import frappe
 
 @frappe.whitelist()
 def get_ip():
+    # ip = get('https://api.ipify.org').text
+    # print(f'My public IP address is: {ip}')
     ip = frappe.local.request_ip
     return ip
