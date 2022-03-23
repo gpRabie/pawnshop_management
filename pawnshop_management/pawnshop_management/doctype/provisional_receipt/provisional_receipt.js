55f7bd24e58ffcf860ddf5b25e2bb10852a1e790
Rabie <gprabiemosessantillan@gmail.com>
2022-03-23 14:25:13 +0800

    Minor bugs fixed in Renewal with Amortization PT

--- pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.js
+++ pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.js
@@ -135,9 +135,9 @@ frappe.ui.form.on('Provisional Receipt', {
 	additional_amortization: function(frm){
 		if (frm.doc.transaction_type == "Renewal w/ Amortization") {
 			calculate_new_interest(frm);
-			console.log(parseFloat(frm.doc.previous_interest_payment));
-			console.log(parseFloat(frm.doc.additional_amortization) + parseFloat(frm.doc.interest_payment) + parseFloat(frm.doc.advance_interest) - parseFloat(frm.doc.discount) - parseFloat(frm.doc.previous_interest_payment));
-			frm.set_value('total', parseFloat(frm.doc.additional_amortization) + parseFloat(frm.doc.interest_payment) + parseFloat(frm.doc.advance_interest) - parseFloat(frm.doc.discount) - parseFloat(frm.doc.previous_interest_payment));
+			// console.log(parseFloat(frm.doc.previous_interest_payment));
+			// console.log(parseFloat(frm.doc.additional_amortization) + parseFloat(frm.doc.interest_payment) + parseFloat(frm.doc.advance_interest) - parseFloat(frm.doc.discount) - parseFloat(frm.doc.previous_interest_payment));
+			frm.set_value('total', (parseFloat(frm.doc.additional_amortization) + parseFloat(frm.doc.interest_payment) + parseFloat(frm.doc.advance_interest)) - parseFloat(frm.doc.discount) - parseFloat(frm.doc.previous_interest_payment));
 			frm.refresh_field('total');
 		} else if (frm.doc.transaction_type == "Amortization") {
 			frm.set_value('total', parseFloat(frm.doc.additional_amortization));
