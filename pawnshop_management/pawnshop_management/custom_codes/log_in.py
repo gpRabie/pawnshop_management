1563917c37ae15b37f21d1a9f849d24c40f7d9cc
Rabie <gprabiemosessantillan@gmail.com>
2022-04-12 09:28:49 +0800

    Secuirty login feature added v.3

--- pawnshop_management/pawnshop_management/custom_codes/log_in.py
+++ pawnshop_management/pawnshop_management/custom_codes/log_in.py
@@ -7,12 +7,19 @@ from frappe.sessions import delete_session
 def login_feed(login_manager):
     ip = frappe.local.request_ip
     user = frappe.get_doc('User', login_manager.user)
+    branch = {
+        "180.195.203.152" : "Garcia's Pawnshop Cavite City Branch",
+        "180.191.229.200" : "Garcia's Pawnshop GTC Branch",
+        "49.144.100.169" : "Garcia's Pawnshop Molino Branch",
+        "49.144.15.239" : "Garcia's Pawnshop Poblacion Branch",
+        "112.210.69.32" : "Garcia's Pawnshop Tanza Branch"
+    }
     if user.role_profile_name == "Cashier" or user.role_profile_name == "Appraiser" or user.role_profile_name == "Vault Custodian" or user.role_profile_name == "Supervisor/Cashier" or user.role_profile_name == "Appraiser/Cashier" or user.role_profile_name == "Supervisor":
         # if ip != "127.0.0.1":
-        if ip == "180.195.203.152" or ip == "180.191.229.200" or ip == "49.144.96.243" or ip == "49.144.15.239" or ip == "112.210.69.32":
+        if ip == "180.195.203.152" or ip == "180.191.229.200" or ip == "49.144.100.169" or ip == "49.144.15.239" or ip == "112.210.69.32":
             frappe.msgprint(
-                msg = 'Welcome ' + user.full_name,
-                title = 'Welcome'
+                msg = 'Welcome, ' + user.full_name,
+                title = 'Welcome to ' + branch[ip]
             )
         # user = frappe.get_doc('user', login_manager.user)
         # print(user.role_profile_name)
