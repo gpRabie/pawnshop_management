25ee72b84158b065e14f57a71aa2d92cf0093e9b
Rabie <gprabiemosessantillan@gmail.com>
2022-04-20 13:28:20 +0800

    Bugs in Non Jewelry Pawn Ticket fixed v.3

--- pawnshop_management/pawnshop_management/doctype/non_jewelry_batch/non_jewelry_batch.py
+++ pawnshop_management/pawnshop_management/doctype/non_jewelry_batch/non_jewelry_batch.py
@@ -10,30 +10,30 @@ class NonJewelryBatch(Document):
 			if self.branch == "Garcia's Pawnshop - CC":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Garcia's Pawnshop - CC")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
 			elif self.branch == "Garcia'a Pawnshop - GTC":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Garcia'a Pawnshop - GTC")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
 			elif self.branch == "Garcia'a Pawnshop - MOL":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Garcia'a Pawnshop - MOL")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
 			elif self.branch == "Garcia'a Pawnshop - POB":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Garcia'a Pawnshop - POB")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
 			elif self.branch == "Garcia'a Pawnshop - TNZ":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Garcia'a Pawnshop - TNZ")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
 			elif self.branch == "Rabie's House":
 				settings = frappe.get_doc('Non Jewelry Naming Series', "Rabie's House")
 				settings.inventory_count += 1
-				settings.item_count = 0
+				settings.item_count = 1
 				settings.save(ignore_permissions=True)
