3bf65ddacf6d4c85bf4693bdce245fb0826080b8
Rabie <gprabiemosessantillan@gmail.com>
2022-04-27 14:14:37 +0800

    Security Login Activated

--- pawnshop_management/pawnshop_management/custom_codes/log_in.py
+++ pawnshop_management/pawnshop_management/custom_codes/log_in.py
@@ -9,14 +9,14 @@ def login_feed(login_manager):
     user = frappe.get_doc('User', login_manager.user)
     branch = {
         "180.195.203.152" : "Garcia's Pawnshop Cavite City Branch",
-        "180.191.232.68" : "Garcia's Pawnshop GTC Branch",
+        "180.190.248.186" : "Garcia's Pawnshop GTC Branch",
         "49.144.100.169" : "Garcia's Pawnshop Molino Branch",
         "49.144.9.203" : "Garcia's Pawnshop Poblacion Branch",
         "136.158.82.68" : "Garcia's Pawnshop Tanza Branch"
     }
     if user.role_profile_name == "Cashier" or user.role_profile_name == "Appraiser" or user.role_profile_name == "Vault Custodian" or user.role_profile_name == "Supervisor/Cashier" or user.role_profile_name == "Appraiser/Cashier" or user.role_profile_name == "Supervisor":
-        if ip != "127.0.0.1":
-        # if ip == "180.195.203.152" or ip == "180.191.232.68" or ip == "49.144.100.169" or ip == "49.144.9.203" or ip == "136.158.82.68":
+        # if ip != "127.0.0.1":
+        if ip == "180.195.203.152" or ip == "180.190.248.186" or ip == "49.144.100.169" or ip == "49.144.9.203" or ip == "136.158.82.68":
             frappe.msgprint(
                 msg = 'Welcome, ' + user.full_name,
                 title = 'Welcome to ' + branch[ip]
