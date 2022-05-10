b42015eedb2798532deb992f387a51f26903487d
Rabie <gprabiemosessantillan@gmail.com>
2022-05-10 09:02:12 +0800

    Make DLG editable in PT

--- pawnshop_management/pawnshop_management/report/in_out_report/in_out_report.py
+++ pawnshop_management/pawnshop_management/report/in_out_report/in_out_report.py
@@ -2,8 +2,45 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _ 
 
 def execute(filters=None):
 	columns, data = [], []
-	data = frappe.db.get_all()
+	columns = get_columns()
+	values = {'company': "TEST Garcia's Pawnshop", 'branch': "Rabie's House"}
+	data = frappe.db.sql("""
+		SELECT 
+			pr.beginning_balance AS start_money,
+			pr.provisional_receipts AS receipt,
+			pr.total_in AS sulod
+		FROM
+			(`tabCash Position Report` pr)
+	""", values=values, as_dict=0)
 	return columns, data
+
+
+def get_columns():
+	columns = [
+		{
+			'fieldname': 'start_money',
+			'label': _('Beginning Balance'),
+			'fieldtype': 'Currency',
+			'width': 120
+		},
+
+		{
+			'fieldname': 'receipt',
+			'label': _('PRovisional Receipt'),
+			'fieldtype': 'Currency',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'sulod',
+			'label': _('Total IN'),
+			'fieldtype': 'Currency',
+			'width': 200
+		}
+	]
+
+	return columns
