ff4d7a4408c5eace3a71ee2b22a73652a4376c6a
Rabie <gprabiemosessantillan@gmail.com>
2022-07-12 15:13:23 +0800

    Bugs fixed in audit form cancellation

--- pawnshop_management/pawnshop_management/doctype/audit_form/audit_form.py
+++ pawnshop_management/pawnshop_management/doctype/audit_form/audit_form.py
@@ -23,7 +23,7 @@ class AuditForm(Document):
 		# if frappe.db.exists(self.document_type, self.document_name) == self.document_name:
 		# 	journal_entry = frappe.db.get_value('Journal Entry', {'reference_document': self.document_name}, 'name')
 		# 	doc1 = frappe.get_doc('Journal Entry', journal_entry)
-		# 	if doc1.docstatus != 1:
+		# 	if doc1.workflow_state == "Audited":
 		# 		doc1.workflow_state = "Draft"
 		# 		doc1.save(ignore_permissions=True)
 		pass
