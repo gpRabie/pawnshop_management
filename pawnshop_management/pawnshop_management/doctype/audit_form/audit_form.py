6b856225e53da5b19befbb3a94580fb3417fff05
Rabie <gprabiemosessantillan@gmail.com>
2022-07-12 14:34:09 +0800

    Audit form bug fixed v.3

--- pawnshop_management/pawnshop_management/doctype/audit_form/audit_form.py
+++ pawnshop_management/pawnshop_management/doctype/audit_form/audit_form.py
@@ -20,10 +20,11 @@ class AuditForm(Document):
 				print("Journal Entry doesn't exist or document isn't linked to any journal entry")
 	
 	def on_cancel(self):
-		if frappe.db.exists(self.document_type, self.document_name) == self.document_name:
-			journal_entry = frappe.db.get_value('Journal Entry', {'reference_document': self.document_name}, 'name')
-			doc1 = frappe.get_doc('Journal Entry', journal_entry)
-			if doc1.docstatus != 1:
-				doc1.workflow_state = "Draft"
-				doc1.save(ignore_permissions=True)
+		# if frappe.db.exists(self.document_type, self.document_name) == self.document_name:
+		# 	journal_entry = frappe.db.get_value('Journal Entry', {'reference_document': self.document_name}, 'name')
+		# 	doc1 = frappe.get_doc('Journal Entry', journal_entry)
+		# 	if doc1.docstatus != 1:
+		# 		doc1.workflow_state = "Draft"
+		# 		doc1.save(ignore_permissions=True)
+		pass
 				
