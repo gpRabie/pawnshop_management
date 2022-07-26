02c6752e932d266df5ba718043c4f1438fd57707
Rabie <gprabiemosessantillan@gmail.com>
2022-07-26 17:38:11 +0800

    Fixing bugs at CPR Daily Balance Add Redeem and Add PP v.5

--- pawnshop_management/pawnshop_management/doctype/cash_position_report/cash_position_report.js
+++ pawnshop_management/pawnshop_management/doctype/cash_position_report/cash_position_report.js
@@ -1166,13 +1166,13 @@ function get_additional_redeem(frm) {
 			let temp_total = 0.00;
 			frm.set_value('additional_redeem', 0.00);
 			for (let index = 0; index < records_pr.length; index++) {
-				// if (records_pr[index].transaction_type == "Redemption") {
-				// 	temp_total += parseFloat(records_pr[index].total)
-				// } else if (records_pr[index].transaction_type == "Renewal") {
-				// 	temp_total += parseFloat(records_pr[index].interest_payment) + parseFloat(records_pr[index].principal_amount)
-				// } else if (records_pr[index].transaction_type == "Renewal w/ Amortization") {
-				// 	temp_total += parseFloat(records_pr[index].interest_payment) + parseFloat(records_pr[index].principal_amount)
-				// }
+				if (records_pr[index].transaction_type == "Redemption") {
+					temp_total += parseFloat(records_pr[index].total)
+				} else if (records_pr[index].transaction_type == "Renewal") {
+					temp_total += parseFloat(records_pr[index].interest_payment) + parseFloat(records_pr[index].principal_amount)
+				} else if (records_pr[index].transaction_type == "Renewal w/ Amortization") {
+					temp_total += parseFloat(records_pr[index].interest_payment) + parseFloat(records_pr[index].principal_amount)
+				}
 			}
 			frm.set_value('additional_redeem', temp_total);
 			frm.refresh_field('additional_redeem');
