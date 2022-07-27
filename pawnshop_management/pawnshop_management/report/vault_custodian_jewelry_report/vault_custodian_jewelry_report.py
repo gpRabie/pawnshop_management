988cb9c641a3db1ea3e30afbc2a8470f35ed80de
Rabie <gprabiemosessantillan@gmail.com>
2022-07-27 16:43:36 +0800

    FIxed CPR Bugs in Daily Balances Add Redeem v.8

--- pawnshop_management/pawnshop_management/report/vault_custodian_jewelry_report/vault_custodian_jewelry_report.py
+++ pawnshop_management/pawnshop_management/report/vault_custodian_jewelry_report/vault_custodian_jewelry_report.py
@@ -9,15 +9,14 @@ def execute(filters=None):
 	columns = get_columns()
 	data2 = frappe.get_all('Pawn Ticket Jewelry', fields=['date_loan_granted'])
 	for i in range(len(data2)):
-		if not date_has_duplicate(data2[i]['date_loan_granted']):
+		if not date_has_duplicate(data2[i]['date_loan_granted'], data):
 			in_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Active'})
 
 			out_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Redeemed'})
 
 			total_count = data2[i]['total_count'] = frappe.db.count('Pawn Ticket Jewelry', {'workflow_state':['Active', 'Expired']})
 
-			data.append({'date_loan_granted': data2[i]['date_loan_granted'], 'in_count': int(in_count), 'out_count': int(out_count), 'total_count': int(total_count)})
-			print(data)
+			data.append({'date_loan_granted': data2[i]['date_loan_granted'], 'in_count': in_count, 'out_count': out_count, 'total_count':total_count})
 	return columns, data
 
 
@@ -53,16 +52,10 @@ def get_columns():
 	]
 	return columns
 
-def date_has_duplicate(date):
+def date_has_duplicate(date, data):
 	has_duplicate = False
-	data = frappe.get_all('Pawn Ticket Jewelry', 
-		filters= {
-			'date_loan_granted': ['<', today()]
-		},
-		fields=['date_loan_granted']
-	)
-	for date_loan_granted in data:
-		if date_loan_granted['date_loan_granted'] == date:
+	for i in range(len(data)):
+		if str(data[i]['date_loan_granted']) == str(date):
 			has_duplicate = True
 			return has_duplicate
 	return has_duplicate
