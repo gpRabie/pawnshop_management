1334e3e89fbe6e95839aaec7736ee95e149dc53b
Rabie <gprabiemosessantillan@gmail.com>
2022-07-29 16:36:58 +0800

    VC Jewelry A debugged v.8

--- pawnshop_management/pawnshop_management/report/vault_custodian_jewelry_report_a/vault_custodian_jewelry_report_a.py
+++ pawnshop_management/pawnshop_management/report/vault_custodian_jewelry_report_a/vault_custodian_jewelry_report_a.py
@@ -6,16 +6,26 @@ import frappe
 def execute(filters=None):
 	columns, data = [], []
 	columns = get_columns()
-	data2 = frappe.get_all('Pawn Ticket Jewelry', filters={'item_series': 'A', 'change_status_date':''}, fields=['date_loan_granted'], order_by='date_loan_granted desc')
+	data2 = frappe.get_all('Pawn Ticket Jewelry', filters={'item_series': 'A'}, fields=['date_loan_granted'], order_by='date_loan_granted desc')
 	for i in range(len(data2)):
 		if not date_has_duplicate(data2[i]['date_loan_granted'], data):
-			in_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Active', 'item_series': 'A', 'change_status_date': ''})
+			in_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Active', 'item_series': 'A'})
+			
+			renewed_count_of_the_day = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Renewed', 'item_series': 'A'})
 
 			out_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': data2[i]['date_loan_granted'], 'workflow_state': 'Redeemed', 'item_series': 'A'})
 
-			total_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': ['<=', data2[i]['date_loan_granted']], 'item_series': 'A', 'workflow_state':['in',['Active', 'Expired']], 'change_status_date': ''})
+			total_count = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': ['<=', data2[i]['date_loan_granted']], 'item_series': 'A', 'workflow_state':['in',['Active', 'Expired']]})
 
-			data.append({'date_loan_granted': data2[i]['date_loan_granted'], 'in_count': in_count, 'out_count': out_count, 'total_count':total_count})
+			total_renewed = frappe.db.count('Pawn Ticket Jewelry', {'date_loan_granted': ['<=', data2[i]['date_loan_granted']], 'item_series': 'A', 'workflow_state':'Renewed'})
+
+			in_for_today = in_count - renewed_count_of_the_day
+			total = total_count - total_renewed
+
+			total_in_for_today = round_up_to_zero(in_for_today)
+			total_pt_count = round_up_to_zero(total)
+
+			data.append({'date_loan_granted': data2[i]['date_loan_granted'], 'in_count': total_in_for_today, 'out_count': out_count, 'total_count':total_pt_count})
 	return columns, data
 
 
@@ -59,5 +69,8 @@ def date_has_duplicate(date, data):
 			return has_duplicate
 	return has_duplicate
 
-# def count_in(name):
-# 	if frappe.db.exists('Pawn Ticket Jewelry', name):
+def round_up_to_zero(num):
+	if int(num) < 0:
+		num = 0
+		return num
+	return num
