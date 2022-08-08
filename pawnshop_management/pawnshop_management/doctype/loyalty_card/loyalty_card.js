41e77dffd24be2e1d2b1eafde8266d012ba581e2
EricMendoza412 <gpericmendoza@gmail.com>
2022-08-08 16:48:54 +0800

    Changed loyalty_card prompt in add pts

--- pawnshop_management/pawnshop_management/doctype/loyalty_card/loyalty_card.js
+++ pawnshop_management/pawnshop_management/doctype/loyalty_card/loyalty_card.js
@@ -24,7 +24,7 @@ frappe.ui.form.on('Loyalty Card', {
 		frm.add_custom_button('Add Points', () => {
 			frappe.prompt([
 				{
-					label: 'Add Points',
+					label: 'Magdagdag tayo ng Pts',
 					fieldname: 'add_points',
 					fieldtype: 'Float'
 				},
