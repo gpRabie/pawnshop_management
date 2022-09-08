c3fd96f46c11dbbb9a56f2709db0d438d5fef630
EricMendoza412 <gpericmendoza@gmail.com>
2022-09-08 14:12:29 +0800

    CPR PR fix

--- pawnshop_management/pawnshop_management/custom_codes/daily_balance.py
+++ pawnshop_management/pawnshop_management/custom_codes/daily_balance.py
@@ -22,6 +22,7 @@ def get_all_additional_pawn(date):
         sum += record
     return sum
 
+@frappe.whitelist()
 def get_all_PR_total(date, branch):
 
     total_pr = frappe.db.get_all('Provisional Receipt', filters={
